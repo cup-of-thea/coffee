58d1ceeb35b3350ba0a2c29b7050ae361844cbf8
Thea <thea.joy@outlook.com>
2022-12-23 17:06:47 +0100

    feat(home) : homepage content

---
+++ lang/fr/home.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'title' => 'Un café ?',
+    'description' =>
+    "Salut, moi c'est Thea, et ce blog est l'espace sur lequel je partage ce que
+    je trouve interessant, opportun, ce qui me fait vibrer, ce qui m'emeut et ce
+    que je veux sortir. C'est donc à la fois un lieu de partage et un exutoire.",
+    'about' =>
+    "Pour info, je suis une meuf trans, donc ce contenu est orienté LGBTQIA+, et
+    donc si cela vous gêne, tant pis, c'est ok, personne ne vous force à lire et
+    rester ici. Vous pouvez fermer cette page, personne ne vous en voudra.",
+    'navigation' =>
+    "Oh, tu es encore là ? Eh bien, installe-toi, tu as deux options, le contenu
+    ouvert à tout le monde (c'est juste en dessous) et le contenu fermé, en gros
+    ce qui est personnel et pour lequel il faut d'abord bien me connaître pour y
+    accéder.",
+    'open_space' => "L'espace ouvert",
+    'closed_space' => "L'espace fermé"
+
+];
